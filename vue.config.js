c280bfb818409845330b61c5c47d719094aa0af4
Vaseem88 <vaseemspi88@gmail.com>
2025-12-10 13:06:30 +0530

    Refactor vue.config.js to use defineConfig

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
-module.exports = {
-    baseUrl: '/',
-    output: {
-    publicPath: 'auto', // Recommended for most simple setups
-  },
-} 
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  
+  // CORRECT: publicPath is at the root level
+  // Use './' (relative) or '' (empty string) to fix the blank page issue
+  publicPath: './', 
+  
+  // INCORRECT: Do not use "output" here
+  // output: { ... } 
+})
 
 /*
 module.exports = {
